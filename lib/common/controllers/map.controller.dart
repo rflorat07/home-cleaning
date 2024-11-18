eceeb85aa4dfe94b9b9ac10405334fd77303c665
Roger Florat <roger.florat@avanade.com>
2024-11-19 00:46:17 +0100

    MapController

---
+++ lib/common/controllers/map.controller.dart
@@ -0,0 +1,243 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapController extends GetxController {
+  static MapController get instance => Get.find();
+
+  GoogleMapController? _mapController;
+
+  RxList<Marker> markers = <Marker>[].obs;
+  final initialPosition = const LatLng(40.712986, -74.004593);
+
+  CameraPosition initialCameraPosition() => CameraPosition(
+        target: initialPosition,
+        zoom: 15,
+      );
+
+  @override
+  void onInit() {
+    super.onInit();
+    _addMarker(initialPosition, 'New York');
+  }
+
+  // Initialize the controller
+  void setMapController(GoogleMapController controller) {
+    _mapController = controller;
+  }
+
+  // Move the camera to a specific position
+  Future<void> moveToPosition(LatLng position) async {
+    if (_mapController != null) {
+      await _mapController!.animateCamera(CameraUpdate.newLatLng(position));
+    }
+  }
+
+  Future<void> _addMarker(LatLng location, String address) async {
+    // Here, you can load your custom marker image
+    BitmapDescriptor customMarker = await BitmapDescriptor.asset(
+      const ImageConfiguration(size: Size(31, 40)), // Set size of the marker
+      'assets/icons/custom_marker.png', // Path to your custom marker image
+    );
+
+    markers.add(
+      Marker(
+        markerId: MarkerId(location.toString()),
+        position: location,
+        //infoWindow: InfoWindow(title: address),
+        icon: customMarker,
+      ),
+    );
+  }
+
+  String loadMapStyle = '''
+[
+    {
+        "featureType": "all",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "saturation": 36
+            },
+            {
+                "color": "#333333"
+            },
+            {
+                "lightness": 40
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#fefefe"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#fefefe"
+            },
+            {
+                "lightness": 17
+            },
+            {
+                "weight": 1.2
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.locality",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "color": "#bd081c"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f5f5f5"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f5f5f5"
+            },
+            {
+                "lightness": 21
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#dedede"
+            },
+            {
+                "lightness": 21
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 29
+            },
+            {
+                "weight": 0.2
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 18
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f2f2f2"
+            },
+            {
+                "lightness": 19
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#e9e9e9"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    }
+]
+''';
+}
--- lib/features/explore/screens/explore.dart
+++ lib/features/explore/screens/explore.dart
@@ -1,9 +1,12 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:iconsax_plus/iconsax_plus.dart';
 
+import '../../../common/common.dart';
+import '../../../common/controllers/map.controller.dart';
+import '../../../utils/utils.dart';
 import '../widgets/explore_header_container.dart';
 import '../widgets/explorer_carousel_card.dart';
 
@@ -12,183 +15,7 @@ class ExploreScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Completer<GoogleMapController> mapController = Completer();
-
-    const CameraPosition kGooglePlex = CameraPosition(
-      target: LatLng(37.42796133580664, -122.085749655962),
-      zoom: 14.4746,
-    );
-
-    String mapOption1 = '''[
-    {
-        "featureType": "all",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "weight": "2.00"
-            }
-        ]
-    },
-    {
-        "featureType": "all",
-        "elementType": "geometry.stroke",
-        "stylers": [
-            {
-                "color": "#9c9c9c"
-            }
-        ]
-    },
-    {
-        "featureType": "all",
-        "elementType": "labels.text",
-        "stylers": [
-            {
-                "visibility": "on"
-            }
-        ]
-    },
-    {
-        "featureType": "landscape",
-        "elementType": "all",
-        "stylers": [
-            {
-                "color": "#f2f2f2"
-            }
-        ]
-    },
-    {
-        "featureType": "landscape",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "color": "#ffffff"
-            }
-        ]
-    },
-    {
-        "featureType": "landscape.man_made",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "color": "#ffffff"
-            }
-        ]
-    },
-    {
-        "featureType": "poi",
-        "elementType": "all",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "all",
-        "stylers": [
-            {
-                "saturation": -100
-            },
-            {
-                "lightness": 45
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "color": "#eeeeee"
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "labels.text.fill",
-        "stylers": [
-            {
-                "color": "#7b7b7b"
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "labels.text.stroke",
-        "stylers": [
-            {
-                "color": "#ffffff"
-            }
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "elementType": "all",
-        "stylers": [
-            {
-                "visibility": "simplified"
-            }
-        ]
-    },
-    {
-        "featureType": "road.arterial",
-        "elementType": "labels.icon",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "transit",
-        "elementType": "all",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "elementType": "all",
-        "stylers": [
-            {
-                "color": "#46bcec"
-            },
-            {
-                "visibility": "on"
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "color": "#c8d7d4"
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "elementType": "labels.text.fill",
-        "stylers": [
-            {
-                "color": "#070707"
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "elementType": "labels.text.stroke",
-        "stylers": [
-            {
-                "color": "#ffffff"
-            }
-        ]
-    }
-]''';
+    final controller = Get.put(MapController());
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
@@ -197,14 +24,16 @@ class ExploreScreen extends StatelessWidget {
       ),
       body: Stack(
         children: [
-          GoogleMap(
-            style: mapOption1,
-            myLocationButtonEnabled: false,
-            initialCameraPosition: kGooglePlex,
-            onMapCreated: (GoogleMapController controller) {
-              mapController.complete(controller);
-            },
-            padding: const EdgeInsets.only(bottom: 24, left: 24),
+          Obx(
+            () => GoogleMap(
+              scrollGesturesEnabled: true,
+              style: controller.loadMapStyle,
+              myLocationButtonEnabled: false,
+              onMapCreated: controller.setMapController,
+              markers: Set<Marker>.of(controller.markers),
+              initialCameraPosition: controller.initialCameraPosition(),
+              padding: const EdgeInsets.only(bottom: 24, left: 24),
+            ),
           ),
           const Positioned(
             top: 0,
@@ -212,11 +41,32 @@ class ExploreScreen extends StatelessWidget {
             left: 0,
             child: TExploreHeaderContainer(),
           ),
-          const Positioned(
+          Positioned(
             bottom: 0,
             right: 0,
             left: 0,
-            child: ExplorerCarouselCard(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(
+                      right: TSizes.defaultSpace,
+                      bottom: TSizes.defaultSpace / 2),
+                  child: TCircularIcon(
+                    width: TSizes.size40,
+                    height: TSizes.size40,
+                    icon: IconsaxPlusBold.gps,
+                    onPressed: () =>
+                        controller.moveToPosition(controller.initialPosition),
+                    showBorder: true,
+                    borderWidth: 0.5,
+                    iconColor: TColors.green,
+                    backgroundColor: Colors.white,
+                  ),
+                ),
+                const ExplorerCarouselCard(),
+              ],
+            ),
           ),
         ],
       ),
